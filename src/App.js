8d675526307b89ad925c42018aa6a9828185198b
srinidhi1071 <srinidhi1071@gmail.com>
2021-01-21 23:03:10 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TheBloodBank from './components/thebloodbank';
+
+function App() {
+  return (
+    <div className="App">
+      <TheBloodBank />
+    </div>
+  );
+}
+
+export default App;
